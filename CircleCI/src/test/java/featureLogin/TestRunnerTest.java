a95abc211420e3e8500a951a04df842fdbeecdcb
Sumit <goyalsumit319@gmail.com>
2022-01-16 18:43:33 +0530

    TEST

---
+++ CircleCI/src/test/java/featureLogin/TestRunnerTest.java
@@ -0,0 +1,22 @@
+package featureLogin;
+
+ 
+import org.junit.runner.RunWith;
+
+//import com.cucumber.listener.ExtentCucumberFormatter;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(stepNotifications=true,monochrome = true,dryRun=false,
+features =
+{"src/test/java/featureLogin/" },
+		   glue = {"stepDefinitions"} )    
+
+public class TestRunnerTest {
+
+ 
+
+}
+
---
+++ CircleCI/src/test/java/stepDefinitions/LoginClass.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class LoginClass {
+
+	
+	 @Given("User login to google")
+	public void user_login_to_google() {
+	    
+	 System.out.println("Hello sumit");
+	 
+	 
+	 }
+
+
+
+}
