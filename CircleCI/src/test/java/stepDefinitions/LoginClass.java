d6272f6de2c3136c3c63175f0650460fcf5ca1c1
sumitgithub007 <66677424+sumitgithub007@users.noreply.github.com>
2022-01-16 19:14:47 +0530

    Update LoginClass.java

--- CircleCI/src/test/java/stepDefinitions/LoginClass.java
+++ CircleCI/src/test/java/stepDefinitions/LoginClass.java
@@ -14,7 +14,7 @@ public class LoginClass {
 	    
  
 	 System.out.println("Hello jalkaj");
-	  WebDriverManager.chromedriver().arch64().setup();
+	 WebDriverManager.chromedriver().arm64().setup();
 	 WebDriver driver = new ChromeDriver();
 	 driver.get("https://www.google.com/");
  	 
